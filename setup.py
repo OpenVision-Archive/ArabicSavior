f536d824d5124988fc712c560ab4af7d7e40ec0b
persianpros <persianpros@yahoo.com>
2021-02-18 15:09:51 +0330

    PEP8 E251 and E252

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 import setup_translate
 
 pkg = 'Extensions.ArabicSavior'
-setup (name = 'enigma2-plugin-extensions-arabicsavior',
-       version = '1.2',
-       description = 'Plugin For Arabic langauge correction.',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['plugin.png', 'font_default.otf', '*/*.png', 'images/*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
+setup (name='enigma2-plugin-extensions-arabicsavior',
+       version='1.2',
+       description='Plugin For Arabic langauge correction.',
+       packages=[pkg],
+       package_dir={pkg: 'plugin'},
+       package_data={pkg: ['plugin.png', 'font_default.otf', '*/*.png', 'images/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass=setup_translate.cmdclass, # for translation
       )
